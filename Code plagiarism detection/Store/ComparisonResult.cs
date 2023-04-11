a3ae1d3c04aed7a02fac6fbddaf00ba4545712d6
FrostyMemes <lager11197@yandex.ru>
2023-04-11 19:19:02 +0300

    feat: added storage for comparision result

---
+++ Code plagiarism detection/Store/ComparisonResult.cs
@@ -0,0 +1,16 @@
+﻿namespace CodePlagiarismDetection
+{
+    public class ComparisonResult
+    {
+        public readonly double Similarity;
+        public readonly FileContent File1;
+        public readonly FileContent File2;
+
+        public ComparisonResult(FileContent file1, FileContent file2, double similarity)
+        {
+            File1 = file1;
+            File2 = file2;
+            Similarity = similarity;
+        }
+    }
+}
