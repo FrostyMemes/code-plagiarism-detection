f8445558382375b6785ce35e5bbe042aa74f6262
FrostyMemes <lager11197@yandex.ru>
2023-04-11 19:15:32 +0300

    feat: created form for setting filter patterns of FileLoader

---
+++ Code plagiarism detection/Forms/FilterForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿
+namespace CodePlagiarismDetection
+{
+    partial class FilterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtFilter = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // txtFilter
+            // 
+            this.txtFilter.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txtFilter.Location = new System.Drawing.Point(0, 25);
+            this.txtFilter.Multiline = true;
+            this.txtFilter.Name = "txtFilter";
+            this.txtFilter.Size = new System.Drawing.Size(287, 320);
+            this.txtFilter.TabIndex = 0;
+            this.txtFilter.Leave += new System.EventHandler(this.txtFilter_Leave);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // FilterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 345);
+            this.Controls.Add(this.txtFilter);
+            this.Name = "FilterForm";
+            this.Text = "Фильтр";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FilterForm_FormClosing);
+            this.Load += new System.EventHandler(this.Filter_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFilter;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
---
+++ Code plagiarism detection/Forms/FilterForm.cs
@@ -0,0 +1,45 @@
+﻿using CodePlagiarismDetection.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodePlagiarismDetection
+{
+    public partial class FilterForm : Form
+    {
+        public FilterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Filter_Load(object sender, EventArgs e)
+        {
+            txtFilter.Text = string.Join(Environment.NewLine, FileLoader.filter);
+        }
+
+        private void txtFilter_Leave(object sender, EventArgs e)
+        {
+            SaveTextToFilterList();
+        }
+
+        private void FilterForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveTextToFilterList();
+            Program.filterForm = null;
+        }
+
+        private void SaveTextToFilterList()
+        {
+            FileLoader.filter = txtFilter.Lines.OfType<string>()
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Distinct()
+                .ToList();
+        }
+    }
+}
