8817ca22b591703de62b94cb9d29e6a61dc4247a
FrostyMemes <lager11197@yandex.ru>
2023-04-11 19:17:08 +0300

    feat: added text normalizer

---
+++ Code plagiarism detection/Services/TextNormalizer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CodePlagiarismDetection.Services
+{
+    public static class TextNormalizer
+    {
+        public static string NormalizeText(string str)
+        {
+            str = string.Join(" ", Regex.Split(str, @"(?:\r\n|\n\r|\n|\r)"));
+            str = str.Replace('\t', ' ');
+            str = Regex.Replace(str,@"\s+"," ");
+            return str.Trim().ToLower();;
+        }
+    }
+}
