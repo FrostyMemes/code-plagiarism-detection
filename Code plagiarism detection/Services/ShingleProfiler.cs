1890af567c306d9ea76abcdc1d8ce36c8354b106
FrostyMemes <lager11197@yandex.ru>
2023-04-11 19:21:28 +0300

    feat: added class for dividing test to shingles

---
+++ Code plagiarism detection/Services/ShingleProfiler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace CodePlagiarismDetection.Services
+{
+    public static class ShingleProfiler
+    {
+        public static int N = 2;
+        public static Dictionary<string, int> GetProfile(string str)
+        {
+            var old = 0;
+            var shingle = String.Empty;
+            var stringWitoutSpaces = Regex.Replace(str,@"\s+"," ");
+            var shingles = new Dictionary<string, int>();
+            
+            for (int i = 0; i < (stringWitoutSpaces.Length-N+1); i++)
+            {
+                shingle = stringWitoutSpaces.Substring(i, N);
+                if (shingles.ContainsKey(shingle))
+                {
+                    old = shingles[shingle];
+                    shingles[shingle] = old + 1;
+                }
+                else
+                    shingles[shingle] = 1;
+            }
+            return shingles;
+        }
+        
+        public static Dictionary<string, int> GetProfile(string str, int n)
+        {
+            var old = 0;
+            var shingle = String.Empty;
+            var stringWitoutSpaces = Regex.Replace(str,@"\s+"," ");
+            var shingles = new Dictionary<string, int>();
+            
+            for (int i = 0; i < (stringWitoutSpaces.Length-n+1); i++)
+            {
+                shingle = stringWitoutSpaces.Substring(i, n);
+                if (shingles.ContainsKey(shingle))
+                {
+                    old = shingles[shingle];
+                    shingles[shingle] = old + 1;
+                }
+                else
+                    shingles[shingle] = 1;
+            }
+            return shingles;
+        }
+    }
+}
