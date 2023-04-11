41ca24311cac57aea1dc78892a6d6e929d602c9b
FrostyMemes <lager11197@yandex.ru>
2023-04-11 19:16:21 +0300

    feat: added class for file loading from set folder

---
+++ Code plagiarism detection/Services/FileLoader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace CodePlagiarismDetection.Services
+{
+    public class FileLoader
+    {
+        public static List<string> filter = new List<string>(); 
+        
+        public static IEnumerable<string> LoadFiles(DirectoryInfo folder, SearchOption option)
+        {
+            if (filter.Count == 0)
+                return folder.GetFiles("*.*", option)
+                .Select(file => file.FullName);
+
+            var fileList = Enumerable.Empty<string>();
+            foreach (var pattern in filter)
+            {
+                fileList = fileList
+                    .Concat(folder.GetFiles(pattern, option)
+                    .Select(file => file.FullName));
+            }
+            return fileList;
+        }
+    }
+}
