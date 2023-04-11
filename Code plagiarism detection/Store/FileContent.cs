fe95d4c16b88811c9ec69a97bda7ab083d0eebc6
FrostyMemes <lager11197@yandex.ru>
2023-04-11 19:16:44 +0300

    feat: added storage for file information

---
+++ Code plagiarism detection/Store/FileContent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using CodePlagiarismDetection.Services;
+
+namespace CodePlagiarismDetection
+{
+    public class FileContent
+    {
+        public readonly string FileName;
+        public readonly string DirectoryName;
+        public readonly string Extension;
+        public readonly string Text;
+        public readonly string NormalizedText;
+        public readonly List<string> Tokens;
+
+        public FileContent(string file)
+        {
+            FileName = Path.GetFileName(file);
+            DirectoryName = Path.GetDirectoryName(file);
+            Extension = Path.GetExtension(file);
+            Text = File.ReadAllText(file);
+            NormalizedText = TextNormalizer.NormalizeText(Text);
+        }
+    }
+}
