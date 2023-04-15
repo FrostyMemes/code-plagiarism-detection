fa850957f9c73856cc9e21dd44bbe6de17d970de
FrostyMemes <lager11197@yandex.ru>
2023-04-16 00:05:52 +0300

    feat: added regex for deleting comments in code text

--- Code plagiarism detection/Services/TextNormalizer.cs
+++ Code plagiarism detection/Services/TextNormalizer.cs
@@ -9,8 +9,12 @@ namespace CodePlagiarismDetection.Services
 {
     public static class TextNormalizer
     {
+        private static readonly string commentAndStringLiterallsPattern = 
+            @"(?x)( ""(?> (?<=@.) (?>[^""]+|"""")*  | (?> [^""\\]+ | \\. )* ) 
+            ""| ' (?> [^'\\]+ | \\. )* ')| // .* | /\* (?s) .*? \*/ ";
         public static string NormalizeText(string str)
         {
+            str = Regex.Replace(str, commentAndStringLiterallsPattern, "");
             str = string.Join(" ", Regex.Split(str, @"(?:\r\n|\n\r|\n|\r)"));
             str = str.Replace('\t', ' ');
             str = Regex.Replace(str,@"\s+"," ");
