286a0aeb779a91ad12048dedf174587b51abb721
FrostyMemes <lager11197@yandex.ru>
2023-04-16 00:06:25 +0300

    test: added test file for regex deleting comments and strings

---
+++ ModuleTests/ServiceTests/FilesForNormalizing/TestRegexForDeletingCommentsAndStringLiterals.c
@@ -0,0 +1,28 @@
+﻿//Test Single Line Comment
+
+/*
+ Test
+ multi
+ line
+ comment
+*/
+
+
+
+
+
+
+"Test two quote string literall"
+
+
+'Test two one string literall'
+
+
+
+//Test Second Single Line Comment
+
+
+
+
+
+"Test Second two quote string literall"
