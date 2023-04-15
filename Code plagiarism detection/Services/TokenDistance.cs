cf04efc055eb7ea254afef1b618066e78211a5d4
FrostyMemes <lager11197@yandex.ru>
2023-04-16 00:05:27 +0300

    feat: added service for processing token distance

---
+++ Code plagiarism detection/Services/TokenDistance.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace CodePlagiarismDetection.Services
+{
+    public static class TokenDistance
+    {
+        public static double GetTokenDistance(string token1, string token2)
+        {
+            var commonLetters = new HashSet<char>(token1);
+            commonLetters.IntersectWith(new HashSet<char>(token2));
+            var allLetters = new HashSet<char>(token1);
+            allLetters.UnionWith(new HashSet<char>(token2));
+            return 1 - commonLetters.Count / (double) allLetters.Count;
+        }
+    }
+}
