8e1ec647c0ca797b799f22922e57c268ce9018a4
FrostyMemes <lager11197@yandex.ru>
2023-04-16 00:25:32 +0300

    feat: added methods

---
+++ Code plagiarism detection/Methods/JaccardCoefficient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using CodePlagiarismDetection.Services;
+
+namespace CodePlagiarismDetection.Methods
+{
+    public class JaccardCoefficient: SimilairyMethod
+    {
+        protected override ComparisonResult CompareFiles(FileContent file1, FileContent file2)
+        {
+            
+            if (file1.NormalizedText== null || file2.NormalizedText == null)
+                throw new ArgumentNullException("String must not be null");
+
+            var s1 = file1.NormalizedText;
+            var s2 = file2.NormalizedText;
+            var similarity = 0.0;
+            if (s1.Equals(s2))
+                similarity = 1.0;
+        
+            if (s1.Length < ShingleProfiler.N || s2.Length < ShingleProfiler.N )
+                similarity = 0.0;
+            
+            var profile1 = ShingleProfiler.GetProfile(s1);
+            var profile2 = ShingleProfiler.GetProfile(s2);
+            HashSet<String> union = new HashSet<string>();
+            union.UnionWith(profile1.Keys);
+            union.UnionWith(profile2.Keys);
+        
+            int inter = profile1.Keys.Count + profile2.Keys.Count
+                        - union.Count;
+            
+            similarity = (double)inter / union.Count;
+            return new ComparisonResult(file1, file2, similarity);
+        }
+    }
+}
---
+++ Code plagiarism detection/Methods/LevenshteinModify.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using CodePlagiarismDetection.Services;
+
+namespace CodePlagiarismDetection.Methods
+{
+    public class LevenshteinModify: SimilairyMethod
+    {
+        protected override ComparisonResult CompareFiles(FileContent file1, FileContent file2)
+        {
+            if (file1.Tokens == null || file2.Tokens == null)
+                throw new ArgumentNullException("String must not be null");
+
+            var s1 = file1.Tokens;
+            var s2 = file2.Tokens;
+            double diff;
+            double[,] m = new double[s1.Count + 1, s2.Count + 1];
+
+            for (int i = 0; i <= s1.Count; i++) { m[i, 0] = i; }
+            for (int j = 0; j <= s2.Count; j++) { m[0, j] = j; }
+
+            for (int i = 1; i <= s1.Count; i++)
+            for (int j = 1; j <= s2.Count; j++)
+            {
+                diff = (s1[i - 1] == s2[j - 1]) 
+                    ? 0 
+                    : TokenDistance.GetTokenDistance(s1[i - 1], s2[j - 1]);
+
+                m[i, j] = Math.Min(Math.Min(m[i - 1, j] + 1,
+                        m[i, j - 1] + 1),
+                    m[i - 1, j - 1] + diff);
+            }
+
+            var distance = m[s1.Count, s2.Count];
+            var normalizedDistance = 1 - (m[s1.Count, s2.Count] / 
+                                          Math.Max(file1.Tokens.Count, file2.Tokens.Count));
+            
+            return new ComparisonResult(file1, file2, normalizedDistance);
+        }
+
+        /*private double[] GetLevinsteinMatrix(List<s>)
+        {
+            
+        }*/
+    }
+}
