f0ea22e9b5192897a14b0ff498a4cb7bda161afc
FrostyMemes <lager11197@yandex.ru>
2023-04-16 00:26:26 +0300

    test: added test for normalizing

---
+++ ModuleTests/ServiceTests/NormalizerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using Xunit;
+
+namespace ModuleTests.ServiceTests
+{
+    public class NormalizerTests
+    {
+        private string direcotryToFileForNormalizing = @"ServiceTests\FilesForNormalizing";
+        private static readonly string commentAndStringLiterallsPattern = 
+            @"(?x)( ""(?> (?<=@.) (?>[^""]+|"""")*  | (?> [^""\\]+ | \\. )* ) 
+            ""| ' (?> [^'\\]+ | \\. )* ')| // .* | /\* (?s) .*? \*/ ";
+        
+        [Theory]
+        [InlineData("TestRegexForDeletingCommentsAndStringLiterals.c")]
+        public void TestRegexForDeletingCommentsAndStringLiterals(string file)
+        {
+            var directory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, 
+                "..", 
+                "..", direcotryToFileForNormalizing);
+            var fileText = File.ReadAllText($"{directory}\\{file}");
+            fileText = Regex.Replace(fileText, commentAndStringLiterallsPattern, String.Empty);
+            Assert.True(String.IsNullOrWhiteSpace(fileText));
+        }
+    }
+}
