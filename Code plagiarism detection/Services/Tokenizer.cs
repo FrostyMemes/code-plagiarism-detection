8610520ab100a946138d3c051f9306b59e6c8682
FrostyMemes <lager11197@yandex.ru>
2023-04-16 00:05:01 +0300

    feat: added code for tokenizing words text

--- Code plagiarism detection/Services/Tokenizer.cs
+++ Code plagiarism detection/Services/Tokenizer.cs
@@ -1,7 +1,23 @@
-﻿namespace CodePlagiarismDetection.Services
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace CodePlagiarismDetection.Services
 {
     public static class Tokenizer
     {
-        
+        private static readonly Regex regex = new Regex(@"(\w+)|(\r?\n)|(.)", 
+            RegexOptions.IgnorePatternWhitespace);
+
+        public static IEnumerable<string> Tokenize(string text)
+        {
+            var matches = regex.Matches(text);
+            foreach (Match match in matches)
+            {
+                if (match.Success)
+                {
+                    yield return match.Value;
+                }
+            }
+        }
     }
 }
