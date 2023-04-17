59711a0c607a7e5cbd242674e79d8c5d93bcdc07
FrostyMemes <lager11197@yandex.ru>
2023-04-17 23:40:19 +0300

    refactor: cleared code

--- Code plagiarism detection/Methods/LevenshteinModify.cs
+++ Code plagiarism detection/Methods/LevenshteinModify.cs
@@ -13,25 +13,24 @@ namespace CodePlagiarismDetection.Methods
 
             var s1 = file1.Tokens;
             var s2 = file2.Tokens;
-            double diff;
-            double[,] m = new double[s1.Count + 1, s2.Count + 1];
+            var diff = 0.0;
+            var m = new double[s1.Count + 1, s2.Count + 1];
 
             for (int i = 0; i <= s1.Count; i++) { m[i, 0] = i; }
             for (int j = 0; j <= s2.Count; j++) { m[0, j] = j; }
 
             for (int i = 1; i <= s1.Count; i++)
-            for (int j = 1; j <= s2.Count; j++)
-            {
-                diff = (s1[i - 1] == s2[j - 1]) 
-                    ? 0 
-                    : TokenDistance.GetTokenDistance(s1[i - 1], s2[j - 1]);
+                for (int j = 1; j <= s2.Count; j++)
+                {
+                    diff = (s1[i - 1] == s2[j - 1]) 
+                        ? 0 
+                        : TokenDistance.GetTokenDistance(s1[i - 1], s2[j - 1]);
 
-                m[i, j] = Math.Min(Math.Min(m[i - 1, j] + 1,
-                        m[i, j - 1] + 1),
-                    m[i - 1, j - 1] + diff);
-            }
-
-            var distance = m[s1.Count, s2.Count];
+                    m[i, j] = Math.Min(Math.Min(m[i - 1, j] + 1,
+                            m[i, j - 1] + 1),
+                        m[i - 1, j - 1] + diff);
+                }
+            
             var normalizedDistance = 1 - (m[s1.Count, s2.Count] / 
                                           Math.Max(file1.Tokens.Count, file2.Tokens.Count));
             
