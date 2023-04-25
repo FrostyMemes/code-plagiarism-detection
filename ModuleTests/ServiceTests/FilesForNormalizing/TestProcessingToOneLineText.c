83a4419e18af6e1e46bf5f8dd6e876f33944ad62
FrostyMemes <lager11197@yandex.ru>
2023-04-26 01:13:14 +0300

    test: added test file for checking normalizing function

---
+++ ModuleTests/ServiceTests/FilesForNormalizing/TestProcessingToOneLineText.c
@@ -0,0 +1,50 @@
+﻿//Test Single Line Comment
+
+/*
+ Test
+ multi
+ line
+ comment
+*/
+
+
+
+Test1
+
+
+
+
+
+
+
+
+
+
+
+Test2
+
+
+
+
+
+Test3
+
+
+dw
+Test4
+                                dwdw
+
+"Test two quote string literall"
+
+
+'Test two one string literall'
+
+
+
+//Test Second Single Line Comment
+
+
+
+
+
+"Test Second two quote string literall"
