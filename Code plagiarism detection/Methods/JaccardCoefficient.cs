c12fffb84553b1025853c9e351fb02ec782d7c35
FrostyMemes <lager11197@yandex.ru>
2023-04-29 22:08:28 +0300

    refactor: changed exception and method returns

--- Code plagiarism detection/Methods/JaccardCoefficient.cs
+++ Code plagiarism detection/Methods/JaccardCoefficient.cs
@@ -6,32 +6,38 @@ namespace CodePlagiarismDetection.Methods
 {
     public class JaccardCoefficient: SimilairyMethod
     {
-        protected override ComparisonResult CompareFiles(FileContent file1, FileContent file2)
+        protected override ComparisonResult CompareFiles(FileContent originalFile, FileContent comparedFile)
         {
             
-            if (file1.NormalizedText== null || file2.NormalizedText == null)
-                throw new ArgumentNullException("String must not be null");
+            if (originalFile.NormalizedText == null) 
+                throw new ArgumentNullException($"{originalFile.FileName} text must not be null");
+            
+            if (comparedFile.NormalizedText == null)
+                throw new ArgumentNullException($"{comparedFile.FileName} text must not be null");
 
-            var s1 = file1.NormalizedText;
-            var s2 = file2.NormalizedText;
+            var s1 = originalFile.NormalizedText;
+            var s2 = comparedFile.NormalizedText;
             var similarity = 0.0;
+            
             if (s1.Equals(s2))
-                similarity = 1.0;
+                return new ComparisonResult(originalFile, comparedFile, 1.0);
         
             if (s1.Length < ShingleProfiler.N || s2.Length < ShingleProfiler.N )
-                similarity = 0.0;
+                return new ComparisonResult(originalFile, comparedFile, 0.0);
             
             var profile1 = ShingleProfiler.GetProfile(s1);
             var profile2 = ShingleProfiler.GetProfile(s2);
-            HashSet<String> union = new HashSet<string>();
+            var union = new HashSet<string>();
             union.UnionWith(profile1.Keys);
             union.UnionWith(profile2.Keys);
         
-            int inter = profile1.Keys.Count + profile2.Keys.Count
+            var inter = profile1.Keys.Count + profile2.Keys.Count
                         - union.Count;
             
-            similarity = (double)inter / union.Count;
-            return new ComparisonResult(file1, file2, similarity);
+            similarity = 1.0 * inter / union.Count;
+            return new ComparisonResult(originalFile, comparedFile, similarity);
         }
+        
+        
     }
 }
