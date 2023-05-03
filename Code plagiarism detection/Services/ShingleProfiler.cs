8e200317d12ef6fa7f5e87f68d48f779552849a8
FrostyMemes <lager11197@yandex.ru>
2023-05-03 23:14:14 +0300

    feat: added shingle profiler creator with custom N-Lenght

--- Code plagiarism detection/Services/ShingleProfiler.cs
+++ Code plagiarism detection/Services/ShingleProfiler.cs
@@ -26,5 +26,25 @@ namespace CodePlagiarismDetection.Services
             }
             return shingles;
         }
+        
+        public static Dictionary<string, int> GetProfile(string text, int n)
+        {
+            var shingle = String.Empty;
+            var stringWitoutSpaces = Regex.Replace(text,@"\s+"," ");
+            var shingles = new Dictionary<string, int>();
+            
+            for (int i = 0; i < (stringWitoutSpaces.Length-n+1); i++)
+            {
+                shingle = stringWitoutSpaces.Substring(i, n);
+                if (shingles.ContainsKey(shingle))
+                {
+                    var old = shingles[shingle];
+                    shingles[shingle] = old + 1;
+                }
+                else
+                    shingles[shingle] = 1;
+            }
+            return shingles;
+        }
     }
 }
