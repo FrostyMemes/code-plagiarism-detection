e5fd29f62158ae2d171ceae974c089873a252456
FrostyMemes <lager11197@yandex.ru>
2023-05-03 23:15:29 +0300

    refactor: changed location of constants

--- Code plagiarism detection/Services/ExcelReportGenerator.cs
+++ Code plagiarism detection/Services/ExcelReportGenerator.cs
@@ -15,19 +15,19 @@ namespace CodePlagiarismDetection.Services
         };
         public static void GenerateExcelReport(DataTable comparisionDataTable, double borderSuspicious)
         {
+            const int START_ROW = 2;
+            const int START_COLUMN = 2;
+            
             var excelApp = new Excel.Application(); excelApp.DisplayAlerts = false;
-            var workBook = excelApp.Workbooks.Add(Type.Missing);
+            var workbook = excelApp.Workbooks.Add(Type.Missing);
             var sheet = (Excel.Worksheet)excelApp.Worksheets.Item[1];
             
             var suspiciousFiles = comparisionDataTable.Select()
                 .Where(row => double.Parse(row["SimilarityPercent"].ToString()) >= borderSuspicious)
                 .ToList();
 
-            const int START_ROW = 2;
-            const int START_COLUMN = 2;
-            
             var columnNameColumIndex = START_COLUMN - 1;
-
+            
             foreach (DataColumn column in comparisionDataTable.Columns)
             {
                 if (!ExceptionColumns.Contains(column.ColumnName))
@@ -44,6 +44,7 @@ namespace CodePlagiarismDetection.Services
 
             var fileInfoRowIndex = START_ROW;
             var fileInfoColumnIndex = START_COLUMN;
+            
             foreach (var fileInfo in suspiciousFiles)
             {
                 fileInfoRowIndex++;
