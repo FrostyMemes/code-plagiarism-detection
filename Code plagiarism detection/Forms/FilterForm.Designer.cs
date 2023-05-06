c5d89e733b3f01ccea177efc4a761f5bc177df56
FrostyMemes <lager11197@yandex.ru>
2023-05-06 22:56:18 +0300

    feat: added label for description of mask feature

--- Code plagiarism detection/Forms/FilterForm.Designer.cs
+++ Code plagiarism detection/Forms/FilterForm.Designer.cs
@@ -29,56 +29,49 @@ namespace CodePlagiarismDetection.Forms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.txtFilter = new System.Windows.Forms.TextBox();
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtFilter
             // 
-            this.txtFilter.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.txtFilter.Location = new System.Drawing.Point(0, 68);
+            this.txtFilter.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFilter.Location = new System.Drawing.Point(6, 84);
             this.txtFilter.Multiline = true;
             this.txtFilter.Name = "txtFilter";
-            this.txtFilter.Size = new System.Drawing.Size(287, 320);
+            this.txtFilter.Size = new System.Drawing.Size(341, 329);
             this.txtFilter.TabIndex = 0;
             this.txtFilter.Leave += new System.EventHandler(this.txtFilter_Leave);
             // 
-            // contextMenuStrip1
+            // lblDescription
             // 
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
-            // 
-            // label1
-            // 
-            this.label1.Location = new System.Drawing.Point(0, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 28);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.lblDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.lblDescription.Location = new System.Drawing.Point(4, 9);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(337, 72);
+            this.lblDescription.TabIndex = 1;
+            this.lblDescription.Text = "На разных строка разместите маски названий файлов, которые хотите обработать.\r\nЧт" + "обы обработать все файлы в выбранной папке - оставтье поле пустым.\r\n";
             // 
             // FilterForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(287, 388);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(352, 419);
+            this.Controls.Add(this.lblDescription);
             this.Controls.Add(this.txtFilter);
             this.Name = "FilterForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Фильтр";
+            this.Text = "Фильтр файлов";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FilterForm_FormClosing);
             this.Load += new System.EventHandler(this.Filter_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
         }
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDescription;
 
         #endregion
 
         private System.Windows.Forms.TextBox txtFilter;
-        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
     }
 }
