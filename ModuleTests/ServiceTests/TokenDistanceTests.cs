88bfca297d026a972c98754b38d165dd430933de
FrostyMemes <lager11197@yandex.ru>
2023-05-06 22:57:17 +0300

    test: added test for TokenDistance.cs

---
+++ ModuleTests/ServiceTests/TokenDistanceTests.cs
@@ -0,0 +1,26 @@
+﻿using CodePlagiarismDetection.Services;
+using Xunit;
+
+namespace ModuleTests.ServiceTests
+{
+    public class TokenDistanceTests
+    {
+        [Fact]
+        public void IsEqual_ShinglesCountWithPropertyNlenght_ReturnTrue()
+        {
+            var token1 = "AAAAAAAAAAAA";
+            var token2 = "BBBBBBBBBBBB";
+            var distance = TokenDistance.GetTokenDistance(token1, token2);
+            Assert.Equal(1, distance);
+        }
+        
+        [Fact]
+        public void IsEqual_ShinglesCountWithPrpertyNlenght_ReturnTrue()
+        {
+            var token1 = "AAAAAAAAAAAA";
+            var token2 = "AAAAAAAAAAAA";
+            var distance = TokenDistance.GetTokenDistance(token1, token2);
+            Assert.Equal(0, distance);
+        }
+    }
+}
