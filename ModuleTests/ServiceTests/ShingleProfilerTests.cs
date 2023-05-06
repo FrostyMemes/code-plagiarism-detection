6e7952fa397680e14df9786c1ce454042ccb0b60
FrostyMemes <lager11197@yandex.ru>
2023-05-06 22:55:37 +0300

    test: added test for testing of ShingleProfiler.cs

--- ModuleTests/ServiceTests/ShingleProfilerTests.cs
+++ ModuleTests/ServiceTests/ShingleProfilerTests.cs
@@ -1,9 +1,47 @@
+using CodePlagiarismDetection.Services;
 using Xunit;
 
 namespace ModuleTests.ServiceTests
 {
     public class ShingleProfilerTests
     {
+        readonly string testStringForCountingShingles = "ABCDEFGH";
+        
+        [Theory]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        public void IsEqual_GenerateCountShingleWithParametrNlenght_ReturnTrue(int shingleLenght)
+        {
+            var profile = ShingleProfiler.GetProfile(testStringForCountingShingles, shingleLenght);
+            var profileShingleCount = profile.Count;
+            Assert.Equal(testStringForCountingShingles.Length - shingleLenght + 1, profileShingleCount);
+        }
+        
+        [Theory]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        public void IsEqual_ShinglesCountWithPropertyNlenght_ReturnTrue(int shingleLenght)
+        {
+            ShingleProfiler.N = shingleLenght;
+            var profile = ShingleProfiler.GetProfile(testStringForCountingShingles);
+            var profileShingleCount = profile.Count;
+            Assert.Equal(testStringForCountingShingles.Length - shingleLenght + 1, profileShingleCount);
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        public void IsEqualToOne_ShinglesCountForStringWithSameCharsEqualToOne_IsTrue(int shingleLenght)
+        {
+            var testString = "AAAAAAAAAAAAAAAAAAAAAAA";
+            var profile = ShingleProfiler.GetProfile(testString, shingleLenght);
+            var profileShingleCount = profile.Count;
+            Assert.Equal(1, profileShingleCount);
+        }
+        
         
     }
 }
