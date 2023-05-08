11847e396b5041d19dcd8ae2708f4a2c776988ea
FrostyMemes <lager11197@yandex.ru>
2023-05-08 20:48:29 +0300

    test: added tests files

---
+++ ModuleTests/ServiceTests/TestFiles/TestExtractLiteralsFromText.c
@@ -0,0 +1,10 @@
+﻿public static IEnumerable<object[]> FilterTestData
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using CodePlagiarismDetection.Services;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+f = 231;
