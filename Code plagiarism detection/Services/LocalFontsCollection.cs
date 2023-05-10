98c43085a21875a4675581bbe4a107a4de1c25ae
FrostyMemes <lager11197@yandex.ru>
2023-05-10 21:47:06 +0300

    feat: added singleton class for custom fonts

---
+++ Code plagiarism detection/Services/LocalFontsCollection.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Drawing.Text;
+
+namespace CodePlagiarismDetection.Services
+{
+    public enum Fonts
+    {
+        MontserattRegular = 0,
+        MontserattMedium = 1,
+        MontserattThin = 2,
+        Dinpro = 3,
+    }
+    
+    public class LocalFontsCollection
+    {
+        private static PrivateFontCollection _instance = null;
+
+        private static readonly Dictionary<Fonts, string> _fonts
+            = new Dictionary<Fonts, string>()
+            {
+                {Fonts.MontserattRegular, @"Fonts\Montserrat-Regular.ttf"},
+                {Fonts.MontserattMedium, @"Fonts\Montserrat-Medium.ttf"},
+                {Fonts.MontserattThin, @"Fonts\Montserrat-Thin.ttf"},
+                {Fonts.Dinpro, @"Fonts\Dinpro.otf"}
+            };
+        
+        private LocalFontsCollection() { }
+
+        private static PrivateFontCollection CreatePrivateFontCollectionInstance()
+        {
+            var privateFontCollection = new PrivateFontCollection();
+            foreach (var font in _fonts)
+                privateFontCollection.AddFontFile(font.Value);
+            
+            return privateFontCollection;
+        }
+        
+        public static PrivateFontCollection GetPrivateFontCollectionInstance()
+        {
+            return _instance ?? (_instance = CreatePrivateFontCollectionInstance());
+        }
+    }
+}
