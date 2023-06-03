58bd1e7126b39788cd33908d6c7832b5c5701830
FrostyMemes <lager11197@yandex.ru>
2023-06-03 14:34:39 +0300

    refactor: added exception processing

--- Code plagiarism detection/Services/ExcelReportGenerator.cs
+++ Code plagiarism detection/Services/ExcelReportGenerator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 
 namespace CodePlagiarismDetection.Services
@@ -18,48 +19,55 @@ namespace CodePlagiarismDetection.Services
             const int START_ROW = 2;
             const int START_COLUMN = 2;
             
-            var excelApp = new Excel.Application(); excelApp.DisplayAlerts = false;
-            var workbook = excelApp.Workbooks.Add(Type.Missing);
-            var sheet = (Excel.Worksheet)excelApp.Worksheets.Item[1];
-            
-            var suspiciousFiles = comparisionDataTable.Select()
-                .Where(row => (double)row["SimilarityPercent"] >= (double)row["CriticalBorderValue"])
-                .ToList();
-
-            var columnNameColumIndex = START_COLUMN - 1;
-            
-            foreach (DataColumn column in comparisionDataTable.Columns)
+            try
             {
-                if (!ExceptionColumns.Contains(column.ColumnName))
+                var excelApp = new Excel.Application(); excelApp.DisplayAlerts = false;
+                var workbook = excelApp.Workbooks.Add(Type.Missing);
+                var sheet = (Excel.Worksheet)excelApp.Worksheets.Item[1];
+                
+                var suspiciousFiles = comparisionDataTable.Select()
+                    .Where(row => (double)row["SimilarityPercent"] >= (double)row["CriticalBorderValue"])
+                    .ToList();
+
+                var columnNameColumIndex = START_COLUMN - 1;
+                
+                foreach (DataColumn column in comparisionDataTable.Columns)
                 {
-                    columnNameColumIndex++;
-                    sheet.Cells[START_ROW, columnNameColumIndex] = column.Caption;
+                    if (!ExceptionColumns.Contains(column.ColumnName))
+                    {
+                        columnNameColumIndex++;
+                        sheet.Cells[START_ROW, columnNameColumIndex] = column.Caption;
+                    }
                 }
+
+                var range = sheet.Range[sheet.Cells[START_ROW, START_COLUMN],
+                    sheet.Cells[START_ROW, columnNameColumIndex]];
+                
+                range.Font.Bold = true;
+
+                var fileInfoRowIndex = START_ROW;
+                var fileInfoColumnIndex = START_COLUMN;
+                
+                foreach (var fileInfo in suspiciousFiles)
+                {
+                    fileInfoRowIndex++;
+                    for (int i = 0; i != comparisionDataTable.Columns.Count; i++)
+                    {
+                        if (!ExceptionColumns.Contains(comparisionDataTable.Columns[i].ColumnName))
+                            sheet.Cells[fileInfoRowIndex, fileInfoColumnIndex + i] = fileInfo[i];
+                    }
+                }
+
+                range = sheet.Range[sheet.Cells[START_ROW, START_COLUMN],
+                    sheet.Cells[fileInfoRowIndex, columnNameColumIndex]];
+                range.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+                range.Columns.AutoFit();
+                excelApp.Visible = true;
             }
-
-            var range = sheet.Range[sheet.Cells[START_ROW, START_COLUMN],
-                sheet.Cells[START_ROW, columnNameColumIndex]];
-            
-            range.Font.Bold = true;
-
-            var fileInfoRowIndex = START_ROW;
-            var fileInfoColumnIndex = START_COLUMN;
-            
-            foreach (var fileInfo in suspiciousFiles)
+            catch (Exception e)
             {
-                fileInfoRowIndex++;
-                for (int i = 0; i != comparisionDataTable.Columns.Count; i++)
-                {
-                    if (!ExceptionColumns.Contains(comparisionDataTable.Columns[i].ColumnName))
-                        sheet.Cells[fileInfoRowIndex, fileInfoColumnIndex + i] = fileInfo[i];
-                }
+                MessageBox.Show("Не удалось создать Excel-документ","Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
-
-            range = sheet.Range[sheet.Cells[START_ROW, START_COLUMN],
-                sheet.Cells[fileInfoRowIndex, columnNameColumIndex]];
-            range.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
-            range.Columns.AutoFit();
-            excelApp.Visible = true;
         }
     }
 }
