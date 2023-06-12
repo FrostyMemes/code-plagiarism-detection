38da24df8560ef94389174c7a43084f33d98e810
FrostyMemes <lager11197@yandex.ru>
2023-06-13 02:04:35 +0300

    feat: added form for about information

---
+++ Code plagiarism detection/Forms/AboutForm.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+
+namespace CodePlagiarismDetection.Forms
+{
+    public partial class AboutForm : Form
+    {
+        public AboutForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AboutForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Program.aboutForm = null; //Удаление ссылки на форму
+        }
+        
+    }
+}
