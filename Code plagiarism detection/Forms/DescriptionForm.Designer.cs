2820f8058fe83b291feee7e756b96e198724aacd
FrostyMemes <lager11197@yandex.ru>
2023-06-13 02:09:14 +0300

    feat: added form for instruction

---
+++ Code plagiarism detection/Forms/DescriptionForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel;
+
+namespace CodePlagiarismDetection.Forms
+{
+    partial class DescriptionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DescriptionForm));
+            this.webBrowserDescription = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // webBrowserDescription
+            // 
+            this.webBrowserDescription.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowserDescription.Location = new System.Drawing.Point(0, 0);
+            this.webBrowserDescription.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowserDescription.Name = "webBrowserDescription";
+            this.webBrowserDescription.Size = new System.Drawing.Size(1108, 767);
+            this.webBrowserDescription.TabIndex = 0;
+            // 
+            // DescriptionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1108, 767);
+            this.Controls.Add(this.webBrowserDescription);
+            this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
+            this.Name = "DescriptionForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Справка";
+            this.Load += new System.EventHandler(this.DescriptionForm_Load);
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.WebBrowser webBrowserDescription;
+
+        #endregion
+    }
+}
---
+++ Code plagiarism detection/Forms/DescriptionForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CodePlagiarismDetection.Forms
+{
+    public partial class DescriptionForm : Form
+    {
+        public DescriptionForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DescriptionForm_Load(object sender, EventArgs e)
+        {
+            var path = Path.Combine(Application.StartupPath, @"Doc\Theory.html"); //Получение пути к справке программы
+            webBrowserDescription.Navigate(path); //Отображение справки на форме
+        }
+    }
+}
