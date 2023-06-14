b42dda73a82a757e7e14dfba8afdb6f5033069c5
FrostyMemes <lager11197@yandex.ru>
2023-06-14 12:54:22 +0300

    feat: added styles and text about program

--- Code plagiarism detection/Forms/AboutForm.Designer.cs
+++ Code plagiarism detection/Forms/AboutForm.Designer.cs
@@ -31,7 +31,18 @@ namespace CodePlagiarismDetection.Forms
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblAbout = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblAbout
+            // 
+            this.lblAbout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblAbout.Location = new System.Drawing.Point(0, 0);
+            this.lblAbout.Name = "lblAbout";
+            this.lblAbout.Size = new System.Drawing.Size(336, 220);
+            this.lblAbout.TabIndex = 0;
+            this.lblAbout.Text = "Программа разработана студентом Мурманского арктического университета\r\nКавериным " + "А.И. ИВБ19о\r\n\r\nПрограмма предназначена для сопоставления исхондых кодов программ" + " на предмет поиска совпадений";
+            this.lblAbout.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // AboutForm
             // 
@@ -39,6 +50,7 @@ namespace CodePlagiarismDetection.Forms
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(336, 220);
+            this.Controls.Add(this.lblAbout);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "AboutForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -46,6 +58,8 @@ namespace CodePlagiarismDetection.Forms
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.AboutForm_FormClosing);
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.Label lblAbout;
 
         #endregion
     }
--- Code plagiarism detection/Forms/AboutForm.cs
+++ Code plagiarism detection/Forms/AboutForm.cs
@@ -1,18 +1,32 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Windows.Forms;
+using CodePlagiarismDetection.Services;
 
 namespace CodePlagiarismDetection.Forms
 {
     public partial class AboutForm : Form
     {
+        private static PrivateFontCollection _privateFontCollection = default;
         public AboutForm()
         {
             InitializeComponent();
+            InitializeComponentStyles();
         }
 
         private void AboutForm_FormClosing(object sender, FormClosingEventArgs e)
         {
             Program.aboutForm = null; //Удаление ссылки на форму
         }
-        
+
+        //Подгрузка внешних шрифтов
+        private void InitializeComponentStyles()
+        {
+            _privateFontCollection = LocalFontsCollection.GetPrivateFontCollectionInstance(); //Получение коллекции внешних добавленных шрифтов
+            
+            foreach (Control control in this.Controls)
+                control.Font = new Font(_privateFontCollection.Families[(int)Fonts.MontserattThin], 10, FontStyle.Regular);
+        }
     }
 }
