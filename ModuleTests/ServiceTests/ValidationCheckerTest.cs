c27929df38dd64dd7bc1f97e4358f2ba3ffdeb9d
FrostyMemes <lager11197@yandex.ru>
2023-06-14 10:14:05 +0300

    feat: added file validation

--- ModuleTests/ServiceTests/ValidationCheckerTest.cs
+++ ModuleTests/ServiceTests/ValidationCheckerTest.cs
@@ -7,15 +7,31 @@ namespace ModuleTests.ServiceTests
 {
     public class ValidationCheckerTest
     {
+        //Тест проверки корректности введенного пути директории
+        [Fact]
+        public void CheckCheckDirectoryExisting_TestCheckingOfValidationOfCurrentDirectoryWithDifferentParameters_ReturnSuccess()
+        {
+            var existingDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory);
+            var notExistingDirectory = "h:\\NotFoundFolder111";
+            var emptyPath = String.Empty;
+            Assert.True(ValidationChecker.CheckDirectoryExisting(existingDirectory, MessageBoxShowMode.Hide));
+            Assert.False(ValidationChecker.CheckDirectoryExisting(notExistingDirectory, MessageBoxShowMode.Hide));
+            Assert.False(ValidationChecker.CheckDirectoryExisting(emptyPath, MessageBoxShowMode.Hide));
+        }
+        
+        //Тест проверки корректности введенного пути файла
         [Fact]
         public void CheckValidationOfCurrentDirectory_TestCheckingOfValidationOfCurrentDirectoryWithDifferentParameters_ReturnSuccess()
         {
-            var existingDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory);
-            var notExistingDirectory = "h:\\NotFoundFolder";
+            var existingFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                "..", "..", 
+                "CodeExamples", "CSharp",
+                "1.c");
+            var notExistingFile = "h:\\NotFoundFolder111\\321.с";
             var emptyPath = String.Empty;
-            Assert.True(ValidationChecker.CheckValidationOfCurrentDirectory(existingDirectory, MessageBoxShowMode.Hide));
-            Assert.False(ValidationChecker.CheckValidationOfCurrentDirectory(notExistingDirectory, MessageBoxShowMode.Hide));
-            Assert.False(ValidationChecker.CheckValidationOfCurrentDirectory(emptyPath, MessageBoxShowMode.Hide));
+            Assert.True(ValidationChecker.CheckFileExisting(existingFile, MessageBoxShowMode.Hide));
+            Assert.False(ValidationChecker.CheckFileExisting(notExistingFile, MessageBoxShowMode.Hide));
+            Assert.False(ValidationChecker.CheckFileExisting(emptyPath, MessageBoxShowMode.Hide));
         }
     }
 }
