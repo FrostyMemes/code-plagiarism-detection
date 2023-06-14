e197dae2bc4b33cf2ff3461ebdb295b64684b6a9
FrostyMemes <lager11197@yandex.ru>
2023-06-14 10:16:59 +0300

    refactor: changed comment and strings literals regex

--- Code plagiarism detection/Services/TextNormalizer.cs
+++ Code plagiarism detection/Services/TextNormalizer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 
@@ -7,19 +8,49 @@ namespace CodePlagiarismDetection.Services
     //Класс для нормализации текста исходного кода
     public static class TextNormalizer
     {
-        //Регулярное выражение для удаление строковых литералов и комментарие
-        public static readonly string commentAndStringLiterallsPattern = 
-            @"(?x)( ""(?> (?<=@.) (?>[^""]+|"""")*  | (?> [^""\\]+ | \\. )* ) 
-            ""| ' (?> [^'\\]+ | \\. )* ')| // .* | /\* (?s) .*? \*/ ";
+        //Регулярное выражение для удаления многострочных комментариев
+        private static readonly List<string> multiCommentsPatterns = new List<string>()
+        {
+            @"""""""(.|[\r\n])*?""""""",
+            @"/\*(.|[\r\n])*?\*/"
+        };
+
+        //Регулярное выражение для удаления однострочных комментариев
+        private static readonly List<string> lineCommentsPatterns = new List<string>()
+        {
+            @"\/\/.*?(?=\r\n|$)",
+            @"#.*?(?=\r\n|$)"
+        };
+        
+        //Регулярное выражение для удаления строковых литералов 
+        private static readonly List<string> stringLiteralsPatterns = new List<string>()
+        {
+            @"\'.*?\'",
+            @"\"".*?\""",
+            @"\`.*?\`",
+        };
         
         //Метод нормализации текста исходного кода
         public static string NormalizeText(string str)
         {
-            str = Regex.Replace(str, commentAndStringLiterallsPattern, "");
+            str = DeleteWithPatterns(str, multiCommentsPatterns);
+            str = DeleteWithPatterns(str, lineCommentsPatterns);
+            str = DeleteWithPatterns(str, stringLiteralsPatterns);
             str = str.Replace(Environment.NewLine, " ");
+            str = DeleteWithPatterns(str, stringLiteralsPatterns);
             str = str.Replace('\t', ' ');
             str = Regex.Replace(str,@"\s+"," ");
             return str.Trim().ToLower();;
+        }
+
+        //Метод удаления элементов по паттернам
+        public static string DeleteWithPatterns(string str, List<string> patterns)
+        {
+            foreach (var pattern in patterns)
+            {
+                str = Regex.Replace(str, pattern, "");
+            }
+            return str;
         }
     }
 }
